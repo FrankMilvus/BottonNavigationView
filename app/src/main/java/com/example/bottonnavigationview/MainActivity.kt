3d0df3373476ea1f767438d362f150a84ecc90a8
FrankMilvus <frankmilvus@gmail.com>
2025-07-24 11:50:13 +0300

    Bottom Navigation

--- app/src/main/java/com/example/bottonnavigationview/MainActivity.kt
+++ app/src/main/java/com/example/bottonnavigationview/MainActivity.kt
@@ -5,8 +5,14 @@ import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import androidx.fragment.app.Fragment
+import com.example.bottonnavigationview.fragments.FavoriteFragment
+import com.example.bottonnavigationview.fragments.HomeFragment
+import com.example.bottonnavigationview.fragments.SearchFragment
+import com.google.android.material.bottomnavigation.BottomNavigationView
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var bottomNavigationView: BottomNavigationView
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
@@ -15,6 +21,34 @@ class MainActivity : AppCompatActivity() {
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
+        }
+
+        bottomNavigationView = findViewById(R.id.nav_view_bottom)
+        supportFragmentManager
+            .beginTransaction()
+            .replace(R.id.container, HomeFragment())
+            .commit()
+
+        bottomNavigationView.setOnItemSelectedListener {
+            var fragment: Fragment? = null
+            when (it.itemId) {
+                R.id.nav_home -> {
+                    fragment = HomeFragment()
+                }
+
+                R.id.nav_favorites -> {
+                    fragment = FavoriteFragment()
+                }
+
+                R.id.nav_search -> {
+                    fragment = SearchFragment()
+                }
+            }
+            supportFragmentManager
+                .beginTransaction()
+                .replace(R.id.container, fragment!!)
+                .commit()
+            return@setOnItemSelectedListener true
         }
     }
 }
